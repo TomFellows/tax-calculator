3ec812cfef3ecd01288a7b1962fb71ebcf9610e8
Tom Fellows <tom@getmercantile.com>
2024-10-21 20:35:37 -0400

    Add results section

---
+++ src/features/results/Results.tsx
@@ -0,0 +1,30 @@
+import { Card, Divider, Stack, Typography } from '@mui/material';
+import { useContext } from 'react';
+import { TaxContext } from '../../context/TaxContext';
+import { getCurrencyFormat } from '../../utils/money';
+import { getEffectiveTaxRate, getTotalTaxesOwed } from '../../utils/tax';
+
+export const Results = () => {
+  const {
+    data: { salary, taxBrackets },
+  } = useContext(TaxContext);
+  const totalTaxesOwed = getCurrencyFormat(getTotalTaxesOwed({ salary, taxBrackets }));
+  const effectiveRate = getEffectiveTaxRate({ salary, taxBrackets });
+
+  return (
+    <Card>
+      <Stack gap={2}>
+        <Typography variant="h5">Your results</Typography>
+        <Typography>Based on your salary and tax brackets, here are your results</Typography>
+        <Divider />
+
+        <Typography>
+          Effective tax rate: <b>{effectiveRate}%</b>
+        </Typography>
+        <Typography>
+          Total taxes owed: <b>{totalTaxesOwed}</b>
+        </Typography>
+      </Stack>
+    </Card>
+  );
+};
--- src/utils/tax.ts
+++ src/utils/tax.ts
@@ -1,16 +1,55 @@
 import { TaxBracket } from '../types/TaxBracket';
-import { getCurrencyFormat } from './money';
 
 /**
  * Get the taxes owed for a specific salary and tax bracket
  * @param bracket: The tax bracket to calculate the taxes owed against
  * @param salary: The salary to calculate the taxes owed for
- * @returns: A string representation of the taxes owed in Canadian currency
+ * @returns: A number representation of the taxes owed in Canadian currency for a given tax bracket
  */
-export const getTaxesOwed = ({ salary, bracket }: { salary: number; bracket: TaxBracket }) => {
+export const getTaxesByBracket = ({ salary, bracket }: { salary: number; bracket: TaxBracket }) => {
   if (salary < bracket.min) {
-    return getCurrencyFormat(0);
+    return 0;
   }
 
-  return getCurrencyFormat(Math.min(salary, bracket.max || 0) * bracket.rate);
+  const taxableSalary = (bracket.max ? Math.min(salary, bracket.max) : salary) - bracket.min;
+
+  return taxableSalary * bracket.rate;
+};
+
+/**
+ * Get the total taxes owed for a specific salary and tax brackets
+ * @param bracket: The tax bracket to calculate the taxes owed against
+ * @param salary: The salary to calculate the taxes owed for
+ * @returns: A number representation of the total taxes owed in Canadian currency
+ */
+export const getTotalTaxesOwed = ({
+  salary,
+  taxBrackets,
+}: {
+  salary: number;
+  taxBrackets: TaxBracket[];
+}) => {
+  return taxBrackets.reduce((total, bracket) => total + getTaxesByBracket({ salary, bracket }), 0);
+};
+
+/**
+ * Get the effective tax rate for a specific salary and tax brackets
+ * @param salary: The salary to calculate the effective tax rate for
+ * @param taxBrackets: The tax brackets to calculate the effective tax rate against
+ * @returns: A number representation of the effective tax rate
+ */
+export const getEffectiveTaxRate = ({
+  salary,
+  taxBrackets,
+}: {
+  salary: number;
+  taxBrackets: TaxBracket[];
+}) => {
+  if (salary === 0) {
+    return 0;
+  }
+
+  const totalTaxesOwed = getTotalTaxesOwed({ salary, taxBrackets });
+
+  return (totalTaxesOwed / salary) * 100;
 };
